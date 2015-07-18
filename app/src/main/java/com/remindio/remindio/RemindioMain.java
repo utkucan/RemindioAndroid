0a1646bbaac169d965222d3e291a7bd0f93c10a6
tkcn <utkucanyucel@gmail.com>
2015-07-19 00:20:17 +0300

    todo
    test

--- app/src/main/java/com/remindio/remindio/RemindioMain.java
+++ app/src/main/java/com/remindio/remindio/RemindioMain.java
@@ -1,23 +1,44 @@
 package com.remindio.remindio;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.parse.Parse;
+import com.parse.ParseACL;
+import com.parse.ParseUser;
 
-public class RemindioMain extends ActionBarActivity {
+public class RemindioMain  extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_remindio_main);
+
+
+        // Initialize Crash Reporting.
+        ParseCrashReporting.enable(this);
+
+        // Enable Local Datastore.
+        Parse.enableLocalDatastore(this);
+
+        // Add your initialization code here
+        Parse.initialize(this, "0IBeBe3cRlWwFD2VobxLAgZF0z5VCq4lTfxDexHb", "8Xxlp5Q5XbsK1resu2A6a9t5gnF9I7fb6yp4XgqW");
+
+
+        ParseUser.enableAutomaticUser();
+        ParseACL defaultACL = new ParseACL();
+        // Optionally enable public read access.
+        // defaultACL.setPublicReadAccess(true);
+        ParseACL.setDefaultACL(defaultACL, true);
+
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_remindio_main, menu);
+//        getMenuInflater().inflate(R.menu.menu_remindio_main, menu);
         return true;
     }
 
